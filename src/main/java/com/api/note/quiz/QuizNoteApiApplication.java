c982b83dc09aa3f74713f56e31288b5b8cfa8329
c5apple <c5apple_party@yahoo.co.jp>
2019-11-23 23:21:23 +0900

    DB接続

--- src/main/java/com/api/note/quiz/QuizNoteApiApplication.java
+++ src/main/java/com/api/note/quiz/QuizNoteApiApplication.java
@@ -1,5 +1,6 @@
 package com.api.note.quiz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * クイズ手帳API
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.api.note.quiz.repository")
 public class QuizNoteApiApplication {
 
 	public static void main(String[] args) {
