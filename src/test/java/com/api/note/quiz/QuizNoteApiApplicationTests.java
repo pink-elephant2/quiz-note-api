c836e6d2361b0c6fd467cfce770a597d4a4f4311
c5apple <c5apple_party@yahoo.co.jp>
2019-12-11 09:28:10 +0900

    UT

---
+++ src/test/java/com/api/note/quiz/QuizNoteApiApplicationTests.java
@@ -0,0 +1,19 @@
+package com.api.note.quiz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * クイズ手帳APIテスト
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuizNoteApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
